347d92e9731889516a31889b698713c5d8533ed4
Alex <enealexandru12@yahoo.com>
2022-09-23 12:29:58 +0300

    interpolation

---
+++ src/main/java/InterpolationSearch.java
@@ -0,0 +1,19 @@
+public class InterpolationSearch {
+    public static void main(String[] args) {
+
+        /*interpolation = improvement over binary search  best used for 'uniformly' distributed data
+                           'guesses' where a value might be based on calculated probe results
+                            if probe is incorrect, search are is narrowed, and a new probe is calculated.
+
+
+                            average case - O(log ( log(n)))
+                            worst case - O(n)[values increase exponentially]
+
+         */
+
+
+
+
+
+    }
+}
